13b347bbc9053074eebccf2d10677a1756299674
unknown <damodar2dikonda@gmail.com>
2020-06-22 17:09:32 +0530

    cmd program basic

---
+++ 22jun20/Rajkumar.java
@@ -0,0 +1,12 @@
+class Rajkumar{
+	
+	public static void main(String movie[]){
+	
+      for(int i=0;i<movie.length;i++){
+
+          System.out.print("movies of Rajkumar:-\t"+movie[i]+"\n");
+      }
+
+
+	}
+}
---
+++ 22jun20/sush.java
@@ -0,0 +1,11 @@
+
+class Sush{
+
+	public static void main(String movies[]){
+
+
+		System.out.println(movies[3]);//output Chhichore
+		//Error if you didnt pass any value:-Exception in thread "main" java.lang.ArrayIndexOutOfBoundsException: 3
+        //at Sush.main(sush.java:7)
+	}
+}
